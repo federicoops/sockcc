5fe0c1effd67db44b4b9d6c5f4770ed2c46a004c
Federico Rossi <13318360+federicoops@users.noreply.github.com>
2025-02-07 08:18:24 +0100

    LICENSE fix

--- src/ServerSocket.cpp
+++ src/ServerSocket.cpp
@@ -1,6 +1,6 @@
 // /*
-// emptyNN
-// Copyright (C) 2024 Federico Rossi
+// sockpp
+// Copyright (C) 2025 Federico Rossi
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,6 @@
 // /*
-// emptyNN
-// Copyright (C) 2024 Federico Rossi
+// sockpp
+// Copyright (C) 2025 Federico Rossi
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // *
+
 
 #include <iostream>
 #include <chrono>
